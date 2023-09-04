0a12a9d3de6f237b5e086ada5be34d3715fc1592
scott <scottchowww@gmail.com>
2023-09-04 13:53:48 +0800

    feat: add logic to generate random RGB values

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ function createSquare(dimension) {
   for (let i = 0; i < dimension * dimension; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.dataset.filled = false;
     square.style.cssText = `height: calc(100% / ${dimension}); width: calc(100% / ${dimension})`;
-    registerEventListener(square);
+    // registerEventListener(square);
     squares.push(square);
   }
   return squares;
@@ -36,9 +37,31 @@ function registerEventListener(node) {
   });
 }
 
+function getRandomNumber(min, max) {
+  let number = Math.floor(Math.random() * (max - min)) + min;
+  return number;
+}
+
+function getRandomRGB() {
+  let red = getRandomNumber(0, 256);
+  let green = getRandomNumber(0, 256);
+  let blue = getRandomNumber(0, 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
 gridSizeBtn.addEventListener("click", () => {
   let gridSize = getGridSize();
   if (gridSize) {
     createGrid(gridSize);
+  }
+});
+
+grid.addEventListener("mouseover", (e) => {
+  if (
+    e.target.classList.contains("square") &&
+    e.target.dataset.filled === "false"
+  ) {
+    e.target.style.backgroundColor = getRandomRGB();
+    e.target.dataset.filled = true;
   }
 });
